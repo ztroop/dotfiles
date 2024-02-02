3aac2c0043408a9747b36dc5bb6babc229c71205
Zackary Troop <zack.tro@gmail.com>
2024-02-02 00:22:08 -0500

    Change cursor

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.opt.guicursor = ""
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
