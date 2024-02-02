bec9f9ebdc0a800529698f2be74d400821abe05a
Zackary Troop <zack.tro@gmail.com>
2024-02-02 14:51:29 -0500

    Minor updates

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,6 +4,8 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.opt.guicursor = ""
+vim.cmd("syntax on")
+vim.o.termguicolors = true
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
--- nvim/sync.sh
+++ nvim/sync.sh
@@ -4,21 +4,57 @@
 SOURCE_DIR="./lua"
 DEST_DIR="$HOME/.config/nvim/lua"
 
-# Check if the source directory exists
-if [ ! -d "$SOURCE_DIR" ]; then
-    echo "Source directory $SOURCE_DIR does not exist. Exiting."
+# Function to copy from source to destination
+copy_to_config() {
+    echo "Copying contents from $SOURCE_DIR to $DEST_DIR"
+    cp -r "$SOURCE_DIR" "$DEST_DIR"
+}
+
+# Function to copy from destination to source
+copy_from_config() {
+    echo "Copying contents from $DEST_DIR to $SOURCE_DIR"
+    cp -r "$DEST_DIR" "$SOURCE_DIR"
+}
+
+# Check for the required argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [to_config | from_config]"
     exit 1
 fi
 
-# Remove the destination directory if it exists
-if [ -d "$DEST_DIR" ]; then
-    echo "Removing existing directory: $DEST_DIR"
-    rm -rf "$DEST_DIR"
-fi
+# Perform the operation based on the argument
+case $1 in
+    to_config)
+        if [ ! -d "$SOURCE_DIR" ]; then
+            echo "Source directory $SOURCE_DIR does not exist. Exiting."
+            exit 1
+        fi
 
-# Copy the contents from the source to the destination
-echo "Copying contents from $SOURCE_DIR to $DEST_DIR"
-cp -r "$SOURCE_DIR" "$DEST_DIR"
+        if [ -d "$DEST_DIR" ]; then
+            echo "Removing existing directory: $DEST_DIR"
+            rm -rf "$DEST_DIR"
+        fi
+
+        copy_to_config
+        ;;
+    from_config)
+        if [ ! -d "$DEST_DIR" ]; then
+            echo "Destination directory $DEST_DIR does not exist. Exiting."
+            exit 1
+        fi
+
+        if [ -d "$SOURCE_DIR" ]; then
+            echo "Removing existing directory: $SOURCE_DIR"
+            rm -rf "$SOURCE_DIR"
+        fi
+
+        copy_from_config
+        ;;
+    *)
+        echo "Invalid argument. Use 'to_config' or 'from_config'."
+        exit 1
+        ;;
+esac
 
 echo "Operation completed."
 
