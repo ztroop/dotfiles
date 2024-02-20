6d696c6f6ed570780ff06bbe0bdb00e39bc476b3
Zackary Troop <zack.tro@gmail.com>
2024-02-19 22:13:15 -0500

    Update zshrc and add setup script

---
+++ setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Function to install ZSH and tools
+install_zsh_and_tools() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # Assuming Mac
+        if ! command -v brew &> /dev/null; then
+            echo "Homebrew not found. Installing Homebrew..."
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        fi
+        echo "Installing/updating tools using Homebrew..."
+        brew install zsh bat tmux eza
+    else
+        # Assuming Linux
+        if command -v apt &> /dev/null; then
+            echo "Using APT to install/update tools..."
+            sudo apt-get update
+            sudo apt-get install -y zsh tmux
+        elif command -v yum &> /dev/null; then
+            echo "Using YUM to install/update tools..."
+            sudo yum update
+            sudo yum install -y zsh tmux
+        else
+            echo "Package manager not supported. Please install ZSH manually."
+            return 1
+        fi
+    fi
+
+    # Install Oh My Zsh
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        echo "Oh My Zsh not found. Installing Oh My Zsh..."
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    else
+        echo "Oh My Zsh is already installed."
+    fi
+}
+
+# Check if zsh is installed; install it if not
+install_zsh_and_tools
+
+# Define ZSH_CUSTOM if it's not already set
+if [ -z "${ZSH_CUSTOM}" ]; then
+    ZSH_CUSTOM=~/.oh-my-zsh/custom
+fi
+
+# Clone zsh-autosuggestions if it doesn't already exist
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+else
+    echo "zsh-autosuggestions is already installed."
+fi
+
+# Clone zsh-syntax-highlighting if it doesn't already exist
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+else
+    echo "zsh-syntax-highlighting is already installed."
+fi
+
+# Check if nvm is installed; install it if not
+if [ -z "${NVM_DIR}" ]; then
+    echo "nvm not found. Installing nvm..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh)"
+else
+    echo "nvm is already installed."
+fi
+
+# Copy the .zshrc file to the home directory
+cp zshrc ~/.zshrc
+# Copy the .tmux.conf file to the home directory
+cp tmux.conf ~/.tmux.conf
+
+echo "Installation and setup completed."
--- zshrc
+++ zshrc
@@ -9,29 +9,15 @@ plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
-create_alias_if_command_exists() {
-    local command=$1
-    local alias_command=$2
-    if command -v "$command" > /dev/null 2>&1; then
-        alias "$alias_command=$command"
-    fi
-}
-
-create_alias_if_command_exists "exa --long --header --git" "ls"
-create_alias_if_command_exists "exa --long --header --git --all" "l"
-create_alias_if_command_exists "bat -p" "cat"
-create_alias_if_command_exists "bat -p" "b"
-create_alias_if_command_exists "nvim" "n"
-create_alias_if_command_exists "nvim" "vim"
-create_alias_if_command_exists "nvim" "vi"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-if [ "$TMUX" = "" ]; then tmux -2; fi
+alias ls="eza --long --header --git --all"
+alias b="bat -p"
+alias vim="nvim"
+alias vi="nvim"
 
 export TERM="xterm-256color"
+export NVM_DIR="$HOME/.nvm"
 
-alias tmux='tmux -2'
-alias tmux='tmux -u'
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+if [ "$TMUX" = "" ]; then tmux -2; fi
